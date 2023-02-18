22fda5f3cec8254c59967987059a7ce9667c2475
Luander <luanderilidio174@gmail.com>
2023-02-18 18:00:22 -0400

    feat: implementa o context na raiz do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { ContextProvider } from "./context/context";
 import AppRoutes from "./Routes/router";
 
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ContextProvider>
+      <AppRoutes />
+    </ContextProvider>
+  );
 }
 
 export default App;
