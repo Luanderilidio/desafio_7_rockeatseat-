a534bd6288611dddbfe0f9546508de45a32d8428
Luander <luanderilidio174@gmail.com>
2023-02-19 14:52:39 -0400

    feat: cria states para botoes dinamicos

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -9,15 +9,18 @@ type ContextProviderProps = {
 interface BlockContextType {
   teste: BlockProps[] | null;
   setTeste: React.Dispatch<React.SetStateAction<BlockProps[] | null>>;
+  view: boolean;
+  setView: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const AppContext = createContext<BlockContextType | null>(null);
 
 export const ContextProvider = ({ children }: ContextProviderProps) => {
   const [teste, setTeste] = useState<BlockProps[] | null>(data);
+  const [view, setView] = useState(true);
 
   return (
-    <AppContext.Provider value={{ teste, setTeste }}>
+    <AppContext.Provider value={{ teste, setTeste, view, setView }}>
       {children}
     </AppContext.Provider>
   );
