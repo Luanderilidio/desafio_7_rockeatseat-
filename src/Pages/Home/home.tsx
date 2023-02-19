07d900027ba404a1b0a3be0df56b0debb3293b5b
Luander <luanderilidio174@gmail.com>
2023-02-19 14:50:40 -0400

    feat: cria logica para botões

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -7,53 +7,60 @@ import Festa1 from "../../assets/festa1.png";
 import { Button } from "@material-tailwind/react";
 import { useContext } from "react";
 import { AppContext } from "../../context/context";
+import { MapTiler } from "../../components/Maptiler";
 
 export default function Home() {
   const testeContext = useContext(AppContext);
 
-  // testeContext?.setTeste([
-  //   {
-  //     id: "asdas",
-  //     title: "O Python do vovô não sobe mais",
-  //     description:
-  //       "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.",
-  //     city: "Mato Grosso",
-  //     uf: "MT",
-  //     image: Festa1,
-  //   },
-  // ]);
-
   return (
     <div className="flex flex-col">
       <Header />
-      <div className="border flex items-center justify-center scrollbar-hide">
+      <div className="flex items-center justify-center scrollbar-hide">
         <div className="my-10 mx-10 sm:mx-0 w-full max-w-6xl grid row-auto items-center grid-cols-3 gap-5">
           <div className="col-span-3 flex items-center justify-between">
             <p className="sm:text-3xl font-Inter font-bold ">
               Blocos Recomendados
             </p>
             <div className="flex items-centes justify-center p-2 gap-2 rounded-lg">
-              <Button size="sm" color="purple">
+              <Button
+                size="sm"
+                variant={testeContext?.view ? "gradient" : "text"}
+                color="purple"
+                onClick={() => {
+                  testeContext?.setView(!testeContext.view);
+                }}
+              >
                 Lista
               </Button>
-              <Button variant="text" color="purple" size="sm">
+              <Button
+                variant={testeContext?.view ? "text" : "gradient"}
+                onClick={() => {
+                  testeContext?.setView(!testeContext.view);
+                }}
+                color="purple"
+                size="sm"
+              >
                 MAPA
               </Button>
             </div>
           </div>
-          {testeContext?.teste?.map((element) => {
-            return (
-              <Block
-                id={element.id}
-                key={element.id}
-                title={element.title}
-                city={element.city}
-                description={element.description}
-                image={element.image}
-                uf={element.uf}
-              />
-            );
-          })}
+          {testeContext?.view ? (
+            testeContext?.teste?.map((element) => {
+              return (
+                <Block
+                  id={element.id}
+                  key={element.id}
+                  title={element.title}
+                  city={element.city}
+                  description={element.description}
+                  image={element.image}
+                  uf={element.uf}
+                />
+              );
+            })
+          ) : (
+            <MapTiler />
+          )}
         </div>
       </div>
     </div>
